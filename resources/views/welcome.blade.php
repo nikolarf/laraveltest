e9d7fac3243a9d1e8495676909e0f51572aee108
Nikola Rafajlovic <nikolarafajlovic@Nikolas-Mac-mini.local>
2019-03-21 13:34:47 +0100

    Second Commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    LaravelTest
                 </div>
 
                 <div class="links">
