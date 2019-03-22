d3392e0782fc9a0641d7b6237cb8dcbd96de2c89
Nikola Rafajlovic <nikolarafajlovic@Nikolas-Mac-mini.local>
2019-03-22 13:51:54 +0100

    change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    LaravelTest1
+                    LaravelTest
                 </div>
 
                 <div class="links">
